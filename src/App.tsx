aec07a00d5cb7c06fed7da32443f96d5b474c469
Lee gangho <rkdgh1190@gmail.com>
2025-01-07 16:45:48 +0900

    init : 브랜치 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="bg-white m-3 font-bold text-2xl">test</h1>;
+  return <h1 className="bg-white m-3 font-bold text-2xl">브랜치 설정t</h1>;
 }
 
 export default App;
