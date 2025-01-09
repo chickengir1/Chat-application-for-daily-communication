a3fdc636da3a5271e0824f059a12318bde647342
Lee gangho <rkdgh1190@gmail.com>
2025-01-09 16:03:30 +0900

    feat: 모달 컴포넌트 구현 및 스토리북 작성
    - Modal 컴포넌트 추가 및 초기 렌더링 상태 제어 기능 구현
    - isOpen 상태를 활용해 모달 열림/닫힘 동작 처리
    - onClose 이벤트로 모달 외부 클릭 및 닫기 버튼 처리
    - 인터렉션 테스트 통과 및 동적 스타일 적용 확인

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from "react";
+
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  modalClassName?: string;
+  contentClassName?: string;
+  overlayClassName?: string;
+  children: ReactNode;
+  size?: "small" | "medium" | "large";
+}
+
+const Modal = ({
+  isOpen,
+  onClose,
+  modalClassName = "",
+  contentClassName = "",
+  overlayClassName = "",
+  children,
+  size = "medium",
+}: ModalProps) => {
+  const sizeClasses = {
+    small: "w-1/4 h-2/4",
+    medium: "w-1/2 h-2/4",
+    large: "w-3/4 h-2/4",
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className={overlayClassName} onClick={onClose}>
+      <div
+        className={` ${sizeClasses[size]} ${contentClassName}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+        <button className={modalClassName} onClick={onClose}>
+          모달 닫기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ src/stories/modal/Modal.stories.tsx
@@ -0,0 +1,52 @@
+import Modal, { ModalProps } from "@/components/common/Modal";
+import { Meta, StoryObj } from "@storybook/react";
+import { expect } from "@storybook/jest";
+import { userEvent, within } from "@storybook/testing-library";
+
+const meta: Meta<typeof Modal> = {
+  title: "hooks/Modal",
+  component: Modal,
+  argTypes: {
+    onClose: { action: "changed" },
+  },
+  parameters: {
+    layout: "centered",
+    docs: {
+      canvas: {},
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<ModalProps>;
+
+export const Default: Story = {
+  args: {
+    isOpen: true,
+    onClose: () => console.log("close"),
+    children: (
+      <div>
+        <h2 className="text-xl font-bold mb-4">Modal Content</h2>
+        <p className="text-gray-700 mb-4">모달 내용</p>
+      </div>
+    ),
+    size: "medium",
+    overlayClassName:
+      "bg-black bg-opacity-50 fixed inset-0 flex items-center justify-center z-50",
+    contentClassName: "bg-white rounded-lg p-6 shadow-lg w-96 relative",
+    modalClassName:
+      "absolute top-4 right-4 text-gray-500 hover:text-gray-800 cursor-pointer bg-blue-500 text-white px-4 py-2 rounded",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // 1) 모달 내용 확인
+    const modalContent = await canvas.findByText("Modal Content");
+    expect(modalContent).toBeInTheDocument();
+
+    // 2) 모달 닫기 버튼 클릭
+    const closeButton = await canvas.findByRole("button");
+    await userEvent.click(closeButton);
+  },
+};
