481130c2e11f125abedc73cfef03a71351c4fcde
kimdaesik <ds500033@naver.com>
2025-01-10 13:02:05 +0900

    feature: IconButton, Badge 컴포넌트 추가

---
+++ src/components/common/IconButton.tsx
@@ -0,0 +1,34 @@
+import testImg from "@/assets/images/test_img.svg";
+
+export interface IconButtonProps {
+  type: "button" | "submit" | "reset";
+  width: string;
+  height: string;
+  src: string;
+  alt: string;
+  disabled: boolean;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const IconButton = ({
+  type = "button",
+  width = "60px",
+  height = "60px",
+  src = testImg,
+  alt,
+  disabled = false,
+  onClick
+}: IconButtonProps) => {
+  return (
+    <button
+      type={type}
+      style={{ width, height }}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <img src={src} alt={alt} className="w-[100%]" />
+    </button>
+  );
+};
+
+export default IconButton;
