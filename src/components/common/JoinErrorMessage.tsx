d8092e116ba97f6651018db0ac7a9d1014f42bdb
kimdaesik <ds500033@naver.com>
2025-01-13 16:54:38 +0900

    chore: 로그인 페이지 주석 추가 및 삭제 /  JoinInput, JoinErrorMessage 컴포넌트 삭제

--- src/components/common/JoinErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-const JoinErrorMessage = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <p className="absolute top-[calc(100%+2px)] text-[11px] text-[#E92B2B]">
-      {children}
-    </p>
-  );
-};
-
-export default JoinErrorMessage;
