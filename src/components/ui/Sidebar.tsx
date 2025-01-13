d79cf95cc1e86bf08529b40099daa43e9f33b6ea
Lee gangho <rkdgh1190@gmail.com>
2025-01-13 12:18:02 +0900

    refactor: 아이콘 활성화 조건 및 클래스 변수를 추출하여 가독성 개선
    - Sidebar 컴포넌트에서 아이콘 활성화 상태를 변수로 추출
    - 버튼 및 텍스트 클래스 조건을 별도 변수로 분리
    - 가독성 및 유지보수성을 향상

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -16,6 +16,8 @@ const Sidebar = () => {
     { icon: <FaBell size={24} />, label: "Notifications" },
     { icon: <FaCog size={24} />, label: "Settings" },
   ];
+  const activeIcon = "w-full border-r-8 bg-[#3D3D3D] border-[#777777]";
+  const activeIconText = "font-bold text-gray-200";
 
   return (
     <div
@@ -28,28 +30,22 @@ const Sidebar = () => {
         <div className="mb-6">
           <div className="bg-[#D9D9D9] rounded-full mt-4 w-16 h-16"></div>
         </div>
-        {icons.map((item, index) => (
-          <button
-            key={item.label}
-            onClick={() => setActiveIndex(index)}
-            className={`p-3 flex flex-col items-center mb-4 ${
-              activeIndex === index
-                ? "w-full border-r-8 bg-[#3D3D3D] border-[#777777]"
-                : "bg-transparent"
-            }`}
-          >
-            {item.icon}
-            <span
-              className={`text-xs mt-1 ${
-                activeIndex === index
-                  ? "font-bold text-gray-200"
-                  : "text-gray-400"
-              }`}
+        {icons.map((item, index) => {
+          const isActive = activeIndex === index;
+          const buttonClass = isActive ? activeIcon : "bg-transparent";
+          const textClass = isActive ? activeIconText : "text-gray-400";
+
+          return (
+            <button
+              key={item.label}
+              onClick={() => setActiveIndex(index)}
+              className={`p-3 flex flex-col items-center mb-4 ${buttonClass}`}
             >
-              {item.label}
-            </span>
-          </button>
-        ))}
+              {item.icon}
+              <span className={`text-xs mt-1 ${textClass}`}>{item.label}</span>
+            </button>
+          );
+        })}
       </div>
       <div className="flex flex-col items-center">
         <button className="p-3 rounded flex flex-col items-center bg-transparent hover:bg-[#3D3D3D] w-full">
