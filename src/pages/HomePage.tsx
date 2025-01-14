543d93c67241922ba06ffd0eedfe4ad477451746
Lee gangho <rkdgh1190@gmail.com>
2025-01-14 17:05:13 +0900

    fix : chatList props 제거 후 확인된 오류 해결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,8 @@ const HomePage = () => {
   return (
     <div className="flex flex-col gap-[16px] h-[100%] py-6 pr-6 text-[#fff]">
       <div className="w-[100%] h-[50%] p-[28px] bg-[#505050] rounded-[8px] overflow-auto">
-        <ChatList title="최근 대화" chats={recentChats} />
+        <h1>최근대화</h1>
+        <ChatList chats={recentChats} />
       </div>
       <div className="w-[100%] h-[50%] p-[28px] bg-[#505050] rounded-[8px] overflow-auto">
         <ContactList title="즐겨찾는 연락처" contacts={contactData} />
