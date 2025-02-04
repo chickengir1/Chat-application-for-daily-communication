781e0743a8581ee4d5c0ee70c56e7ce8f5cff6f8
Lee gangho <rkdgh1198@gmail.com>
2025-02-04 13:55:43 +0900

    feat : 이모지 피커 구현 #22

--- src/components/feature/chat/message/MessageInput.tsx
+++ src/components/feature/chat/message/MessageInput.tsx
@@ -1,12 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState, Suspense } from "react";
 import Input from "@/components/common/Input";
 import { useFileUpload } from "@/hooks/feature/chat/message/useFileUpload";
 import useMessageInput from "@/hooks/feature/chat/message/useMessageInput";
 import { FaPaperclip, FaSmile, FaPaperPlane } from "react-icons/fa";
+const EmojiPicker = React.lazy(() => import("emoji-picker-react"));
 
 const MessageInput = ({ roomId }: { roomId: string }) => {
   const { value, onChange, onKeyDown, handleSendMessage, setValue } =
     useMessageInput(roomId);
   const { handleFileChange, isUploading } = useFileUpload(roomId, setValue);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+  const handleEmojiClick = (emojiObject: any) => {
+    setValue(value + emojiObject.emoji);
+    setShowEmojiPicker(false);
+  };
 
   return (
     <div className={styles.container}>
@@ -22,14 +31,27 @@ const MessageInput = ({ roomId }: { roomId: string }) => {
           disabled={isUploading}
         />
       </label>
-      <button className={styles.iconButton} disabled={isUploading}>
+      <button
+        className={styles.iconButton}
+        onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+        disabled={isUploading}
+      >
         <FaSmile size={20} />
       </button>
+      {showEmojiPicker && (
+        <Suspense fallback={<div>이모지 불러오는 중...</div>}>
+          <div className="absolute bottom-16 left-4">
+            <EmojiPicker onEmojiClick={handleEmojiClick} />
+          </div>
+        </Suspense>
+      )}
       <Input
         value={value}
         onChange={onChange}
         onKeyDown={onKeyDown}
-        placeholder={isUploading ? "파일 업로드 중..." : "Type a message..."}
+        placeholder={
+          isUploading ? "파일 업로드 중..." : "메시지를 입력하세요..."
+        }
         className={styles.textInput}
         maxLength={100}
         disabled={isUploading}
@@ -48,12 +70,12 @@ const MessageInput = ({ roomId }: { roomId: string }) => {
 export default MessageInput;
 
 const styles = {
-  container: "flex items-center rounded-lg bg-[#404040] p-3",
+  container: "flex items-center rounded-lg bg-[#404040] p-3 relative",
   label: "cursor-pointer text-gray-400 hover:text-white",
   hiddenInput: "hidden",
   textInput:
     "mx-3 flex-1 rounded-lg bg-[#303030] px-3 py-2 text-white focus:outline-none disabled:opacity-50",
-  iconButton: "ml-4 text-gray-400 hover:text-white disabled:opacity-50",
+  iconButton: "text-gray-400 hover:text-white ml-2",
   sendButton:
     "rounded-lg p-2 text-white transition duration-200 ease-in-out hover:bg-gray-600 disabled:opacity-50",
 };
